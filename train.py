e479fac160be676b7e005bf90771c0c45dd28002
Emanuele Forlano <emanuele.forlano@gmail.com>
2021-01-09 11:09:16 +0000

    train from vm

--- train.py
+++ train.py
@@ -15,16 +15,6 @@ from azureml.core import Workspace, Datastore, Dataset
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = Dataset.Tabular.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -51,7 +41,19 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+
+    return x_df, y_df
+
+ds = Dataset.Tabular.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
+### YOUR CODE HERE ###a
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
